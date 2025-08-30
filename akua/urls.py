81d4a9d73983463fe4f92dcf19467212210d76d2
Luis Agudelo <laagudelor@eafit.edu.co>
2025-08-30 18:33:42 -0500

    Primer commit: proyecto Akua

---
+++ akua/urls.py
@@ -0,0 +1,31 @@
+"""
+URL configuration for akua project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/5.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+# akua/urls.py
+from django.contrib import admin
+from django.urls import path
+from usuarios import views  # importa tus vistas de la app usuarios
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('login/', views.login_view, name='login'),
+    path('logout/', views.logout_view, name='logout'),
+    path('dashboard/', views.dashboard_view, name='dashboard'),
+
+    # esta es la raíz
+    path('', views.dashboard_view, name='index'),
+    path('donde-ir/', views.donde_ir_view, name='donde_ir'),
+]
